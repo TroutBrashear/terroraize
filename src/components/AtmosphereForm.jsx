ed1e8814e9dd4f1c9bbc90cf4b3c3320528b1ea9
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2025-08-19 20:05:52 -0400

    feat: created form to edit atmosphere settings

---
+++ src/components/AtmosphereForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { useSettingStore } from '../state/settingStore';
+import styles from './Form.module.css';
+
+function AtmosphereForm({onSaveComplete}) {
+	const updateAtmoSettings = useSettingStore((state) => state.setAtmoSettings);
+	
+	const text = useSettingStore((state) => state.writerSettings.atmosphere.text);
+	const resetEachTurn = useSettingStore((state) => state.writerSettings.atmosphere.resetEachTurn);
+	
+	const [atmoText, setAtmoText] = useState(text || '');
+	const [resetToggle, setResetToggle] = useState(resetEachTurn || true);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		
+		const newAtmoSettings = {
+			text: atmoText,
+			resetEachTurn: resetToggle,	
+		};
+		
+		updateAtmoSettings(newAtmoSettings);
+		
+		onSaveComplete();
+	};
+
+	const handleCheckboxChange = (event) => {
+ 
+		const shouldReset = event.target.checked; 
+
+		setResetToggle(shouldReset);
+	};
+
+	return (
+		<form className={styles.form} onSubmit={handleSubmit}>
+			<h2> Atmosphere Settings </h2>
+			
+			<input className={styles.input} value={atmoText} onChange={(e) => setAtmoText(e.target.value)} />
+			
+			<input type="checkbox" id="reset-atmo-toggle" checked={resetToggle} onChange={handleCheckboxChange}/>
+			
+			<button className={styles.submitButton} type="submit">Submit</button>
+		</form>
+	);
+
+}
+
+export default AtmosphereForm;
